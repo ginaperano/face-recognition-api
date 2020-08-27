64ad66aaa52f05f890220308fb5e494f2b70128b
Gina <hello@ginaperano.com>
2020-08-27 12:18:09 -0700

    .gitignore node_modules

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 const database = {
     users: [
         {
