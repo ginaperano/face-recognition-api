f1472221e49010b6fa777fb1970dab0af5a7e873
Gina <hello@ginaperano.com>
2020-08-27 11:22:47 -0700

    backend local tests - signin working

---
+++ server.js
@@ -0,0 +1,97 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const app = express();
+
+app.use(bodyParser.json());
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get('/', (req, res) => {
+    res.send('this is working');
+})
+
+app.post('/signin', (req, res) => {
+    if (req.body.email === database.users[0].email &&
+        req.body.password === database.users[0].password) {
+        res.json('success');
+    } else {
+        res.status(400).json('error logging in');
+    }
+})
+
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    database.users.push({
+        id: '125',
+        name: 'name',
+        email: 'email',
+        password: 'password',
+        entries: 0,
+        joined: new Date()
+    })
+    res.json(database.users[database.users.length - 1])
+})
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.post('/image', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.listen(3001, () => {
+    console.log('app is running on port 3001')
+})
+
+
+/**
+ *
+ * --> res = this is working
+ * /signin --> POST = success/fail
+ * /register --> POST = user
+ * /profile/:userId --> GET = user
+ * /image --> PUT --> user
+ *
+ */
