e9af817125e897e09db66b8de1877a4a9eba55c3
Gina <hello@ginaperano.com>
2020-08-29 11:52:44 -0700

    setting up database to test cluster

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 
 const express = require("express");
 const app = express();
-const port = process.env.PORT || 3001;
 const mongoose = require('mongoose');
+const db = require("./config/keys").mongoURI;
+
+
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    })
+    .then(() => console.log('DB Connected!'))
+    .catch(err => {
+        console.log('DB Connection Error');
+    });
 
 app.get("/", (req, res) => res.send("Hello"));
 
 
 
 
-
+const port = process.env.PORT || 3001;
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
