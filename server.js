a83e7acc95c12f90e6c6299e22b17acc6e10b301
Gina <hello@ginaperano.com>
2020-08-29 12:21:37 -0700

    removed mongoDB application connection

--- server.js
+++ server.js
@@ -1,19 +1,9 @@
 
 const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const db = require("./config/keys").mongoURI;
 
 
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useUnifiedTopology: true,
-        useNewUrlParser: true,
-    })
-    .then(() => console.log('DB Connected!'))
-    .catch(err => {
-        console.log('DB Connection Error');
-    });
+
 
 app.get("/", (req, res) => res.send("Hello"));
 
