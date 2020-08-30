35cc26965d2a49497eaea018199c8eddb51b9b04
Gina <hello@ginaperano.com>
2020-08-30 13:47:08 -0700

    testing and registering first user

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
-const postgres = knex({
+const db = knex({
     client: 'pg',
     connection: {
         host: '127.0.0.1', //localhost
@@ -16,7 +16,9 @@ const postgres = knex({
     }
 });
 
-console.log(postgres.select('*').from('users'));
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
 
 
 const app = express();
@@ -60,13 +62,11 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    database.users.push({
-        id: '125',
-        name: name,
+    db('users').insert({
         email: email,
-        entries: 0,
+        name: name,
         joined: new Date()
-    })
+    }).then(console.log)
     res.json(database.users[database.users.length - 1]);
 })
 
