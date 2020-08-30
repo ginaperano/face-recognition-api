3f12423f52af7639dbb8dd4b7a0d1f9948822bfb
Gina <hello@ginaperano.com>
2020-08-29 20:07:47 -0700

    setting up rank route

--- server.js
+++ server.js
@@ -59,14 +59,40 @@ app.post('/register', (req, res) => {
     res.json(database.users[database.users.length - 1]);
 })
 
+// get user is working
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            return res.json(user);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
+app.post('/image', (req, res) => {
+    const { id } = req.body;
+    let found = false;
+    database.users.forEach(user => {
+        if (user.id === id) {
+            found = true;
+            user.entries++
+            return res.json(user.entries);
+        }
+    })
+    if (!found) {
+        res.status(400).json('not found');
+    }
+})
+
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
 
 /*
-/ root route --> res = this is working
-/ signin --> POST = success/fail
-/ register --> POST = user
-/ profile/:userId --> GET = user
 / image --> PUT --> user
 */
