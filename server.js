3c7702e47fcce9cc0e7959751415369e20ecd54e
Gina <hello@ginaperano.com>
2020-08-29 21:56:56 -0700

    storing user passwords

--- server.js
+++ server.js
@@ -52,6 +52,9 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
+    bcrypt.hash(password, null, null, function (err, hash) {
+        console.log(hash);
+    });
     database.users.push({
         id: '125',
         name: name,
@@ -93,18 +96,17 @@ app.post('/image', (req, res) => {
     }
 })
 
-bcrypt.hash(password, null, null, function (err, hash) {
-    console.log(hash);
-    // Store hash in your password DB.
-});
+
 
 // Load hash from your password DB.
+/*
 bcrypt.compare("bacon", hash, function (err, res) {
     // res == true
 });
 bcrypt.compare("veggies", hash, function (err, res) {
     // res = false
 });
+*/
 
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
