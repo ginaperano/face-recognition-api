77520d7c1a32a02e5c8afba0b33ddf1caa144049
Gina <hello@ginaperano.com>
2020-08-29 19:24:35 -0700

    set up psql database and express server

--- server.js
+++ server.js
@@ -1,15 +1,52 @@
 
-const express = require("express");
-const app = express();
-
-
-
-
-app.get("/", (req, res) => res.send("Hello"));
-
-
-
-
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcyrptjs');
+const cors = require('cors');
+const knex = require('knex');
 const port = process.env.PORT || 3001;
 
+
+const app = express();
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const database = {
+    users: [
+        {
+            id: '123',
+            name: 'John',
+            email: 'john@gmail.com',
+            password: 'cookies',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name: 'Sally',
+            email: 'sally@gmail.com',
+            password: 'bananas',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
+
+app.get("/", (req, res) => res.send("server is working!"));
+
+app.post('/signin', (req, res) => {
+    res.json("signin")
+})
+
+
+
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+/*
+/ root route --> res = this is working
+/ signin --> POST = success/fail
+/ register --> POST = user
+/ profile/:userId --> GET = user
+/ image --> PUT --> user
+*/
