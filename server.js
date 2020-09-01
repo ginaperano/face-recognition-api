7c308e8555395289b8ea23efb2ed525b9f3b4a86
Gina <hello@ginaperano.com>
2020-09-01 08:50:28 -0700

    registering user updates

--- server.js
+++ server.js
@@ -62,16 +62,31 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
     const { email, name, password } = req.body;
-    db('users')
-        .returning('*')
-        .insert({
-            email: email,
-            name: name,
-            joined: new Date()
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+        trx.insert({
+            hash: hash,
+            email: email
         })
-        .then(user => {
-            res.json(user[0]);
-        })
+            .into('login')
+            .returning('email')
+            .then(loginEmail => {
+                return trx('users')
+                    .returning('*')
+                    .insert({
+                        email: loginEmail[0],
+                        name: name,
+                        joined: new Date()
+                    })
+                    .then(user => {
+                        res.json(user[0]);
+
+                    })
+            })
+            .then(trx.commit)
+            .catch(trx.rollback)
+    })
+
         .catch(err => res.status(400).json('account already exists'))
 })
 
@@ -96,8 +111,9 @@ app.put('/image', (req, res) => {
         .increment('entries', 1)
         .returning('entries')
         .then(entries => {
-            console.log(entries);
+            res.json(entries[0]);
         })
+        .catch(err => res.status(400).json('unable to get entries'))
 })
 
 
