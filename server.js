4dd1b20b6c14e93a88900b45a5e5ef0225ef5f84
Gina <hello@ginaperano.com>
2020-09-02 13:47:15 -0700

    updating and testing all routes

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-
 const db = knex({
     client: 'pg',
     connection: {
@@ -22,65 +21,17 @@ const db = knex({
     }
 });
 
-db.select('*').from('users').then(data => {
-    console.log(data);
-});
-
-
 const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors())
 
-
-
-app.get("/", (req, res) => res.send());
-
-
-app.post('/signin', (req, res) => {
-    db.select('email', 'hash').from('login')
-        .where('email', '=', req.body.email)
-        .then(async data => {
-            const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
-            if (isValid) {
-                try {
-                    const user = await db.select('*').from('users')
-                        .where('email', '=', req.body.email);
-                    res.json(user[0]);
-                }
-                catch (err) {
-                    return res.status(400).json('user does not exist');
-                }
-            } else {
-                res.status(400).json('wrong credentials')
-            }
-        })
-        .catch(err => res.status(400).json('wrong credentials'))
-})
-
-
+app.get("/", (req, res) => { res.send(db.users) })
+app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
-
-
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            } else {
-                res.status(400).json('Not found')
-            }
-        })
-        .catch(err => res.status(400).json('User does not exist'))
-})
-
-
+app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db) })
 app.put('/image', (req, res) => { image.handleImage(req, res, db) })
-
-
-
-
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
